fd84eff90b758f28d4b1278814610be34181022b
Bartek <bartekma89@gmail.com>
2018-02-01 13:19:12 +0100

    added methods of life cycle

--- components/Counter.js
+++ components/Counter.js
@@ -1,9 +1,17 @@
 var Counter = React.createClass({
 
+    getDefaultProps: function() {
+        console.log('Default props...');
+    },
+
     getInitialState: function() {
         return {
             counter: 0
         }
+    },
+
+    componentWillMount: function() {
+        console.log('Component is mounting')
     },
 
     increment: function() {
@@ -24,6 +32,27 @@ var Counter = React.createClass({
             React.createElement('button', { onClick: this.decrement }, 'Decrement'),
             React.createElement('span', {}, 'Result -> ' + this.state.counter)
         );
+    },
+
+    componentDidMount: function() {
+        console.log('Component mounted')
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        console.log('Component will receive props ', nextProps);
+    },
+
+    shouldComponentUpdate: function(nextProps, nextState) {
+        console.log('should component update -> ', nextState.counter);
+        return nextState.counter === this.state.counter;
+    },
+
+    componentWillUpdate: function(nextProps, nextState) {
+        console.log('component will update -> ', nextState.counter);
+    },
+
+    componentDidUpdate: function(prevProps, prevState) {
+        console.log('component did update -> ', prevState.counter);
     }
 
 });
